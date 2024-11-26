91ccd8b216b755da072221c75e77ebc0c8b1eb18
Andrea Bergia <andreabergia@gmail.com>
2024-11-26 18:17:54 +0100

    Removed horizontal terminal shortcut

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -4,12 +4,5 @@ return {
     require('toggleterm').setup {
       open_mapping = [[<C-/>]],
     }
-
-    -- TODO it's super small
-    local Terminal = require('toggleterm.terminal').Terminal
-    local vertical_term = Terminal:new { direction = 'vertical', count = 2 }
-    vim.keymap.set('n', '<C-.>', function()
-      vertical_term:toggle()
-    end, { desc = 'Open terminal horizontally' })
   end,
 }

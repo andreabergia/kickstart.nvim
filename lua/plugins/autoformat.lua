29fdaebb2abc425fef0a0ac4ea7af6579176e779
andrea.bergia <andrea.bergia@servicenow.com>
2024-11-26 17:50:05 +0100

    Disallow autoformat for javascript

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -19,16 +19,18 @@ return {
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
+      local disable_lsp_filetypes = { c = true, cpp = true }
       local lsp_format_opt
-      if disable_filetypes[vim.bo[bufnr].filetype] then
+      if disable_lsp_filetypes[vim.bo[bufnr].filetype] then
         lsp_format_opt = 'never'
       else
         lsp_format_opt = 'fallback'
       end
+      local force_dry_run_filetypes = { javascript = true }
       return {
         timeout_ms = 500,
         lsp_format = lsp_format_opt,
+        dry_run = force_dry_run_filetypes[vim.bo[bufnr].filetype],
       }
     end,
     formatters_by_ft = {
@@ -37,7 +39,8 @@ return {
       -- python = { "isort", "black" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      rust = { 'rust-analyzer' },
     },
   },
 }

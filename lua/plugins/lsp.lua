216b88f244970dd7f29fedcada1b755d3250f168
andrea.bergia <andrea.bergia@servicenow.com>
2024-11-26 17:50:17 +0100

    Always install lsp for rust and js/ts

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -180,7 +180,8 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
+        eslint = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:

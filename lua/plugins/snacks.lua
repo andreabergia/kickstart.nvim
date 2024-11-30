873bff6011833f3412cfc57d8ffc8d122892ef0e
Andrea Bergia <andreabergia@gmail.com>
2024-11-30 17:49:00 +0100

    Improved dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,24 +3,26 @@ return {
   'folke/snacks.nvim',
   opts = {
     dashboard = {
-      preset = {
-        header = [[
-  _   ___      _______ __  __ 
- | \ | \ \    / /_   _|  \/  |
- |  \| |\ \  / /  | | | \  / |
- | . ` | \ \/ /   | | | |\/| |
- | |\  |  \  /   _| |_| |  | |
- |_| \_|   \/   |_____|_|  |_|
- ]],
-        keys = {
-          { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
-          { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
-          { icon = '󰒲 ', key = 'l', desc = 'Lazy', action = ':Lazy' },
-          { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+      sections = {
+        { section = 'header' },
+
+        {
+          pane = 2,
+          section = 'terminal',
+          cmd = 'colorscript -e zwaves',
+          height = 7,
+          padding = 1,
         },
+
+        {
+          icon = ' ',
+          title = 'Keymaps',
+          section = 'keys',
+          padding = 1,
+        },
+        { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1, pane = 2 },
+        { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1, pane = 2 },
+        { section = 'startup' },
       },
     },
   },

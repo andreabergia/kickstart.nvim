24735b8c345e3a35a759e62f40b3240456e69513
Andrea Bergia <andreabergia@gmail.com>
2025-01-04 14:23:58 +0100

    Added <C-K> binding to show signature help

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -120,6 +120,8 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
 
+          map('<C-k>', vim.lsp.buf.signature_help, 'Signature help')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed

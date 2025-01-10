a2f34f683ec3b38e91477aeca694acd931feaad2
Andrea Bergia <andreabergia@gmail.com>
2025-01-10 21:57:54 +0100

    Removed snippets for markdown

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -22,7 +22,9 @@ return {
         {
           'rafamadriz/friendly-snippets',
           config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
+            require('luasnip.loaders.from_vscode').lazy_load {
+              exclude = { 'markdown' },
+            }
           end,
         },
       },

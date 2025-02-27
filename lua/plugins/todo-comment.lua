0620489799e68a02c3366e66aff4e18cfb9b2f5f
Andrea Bergia <andreabergia@gmail.com>
2025-02-27 14:54:46 +0100

    Made `]t` be tab next and moved next todo to `]o`

--- lua/plugins/todo-comment.lua
+++ lua/plugins/todo-comment.lua
@@ -4,8 +4,8 @@ return {
   opts = {},
   -- stylua: ignore
   keys = {
-    { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
-    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+    { "]o", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+    { "[o", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
     { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo" },
     { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
   },

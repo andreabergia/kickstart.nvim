8655b11176fe420dbb0aa7515df538874b6b8673
Andrea Bergia <andreabergia@gmail.com>
2025-03-03 15:47:22 +0100

    Disable project auto-detect for servicenow stuff

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,6 +1,11 @@
 return {
   'ahmedkhalf/project.nvim',
   config = function()
-    require('project_nvim').setup {}
+    require('project_nvim').setup {
+      exclude_dirs = {
+        -- ServiceNow projects
+        '/Users/andrea.bergia/fluent',
+      },
+    }
   end,
 }

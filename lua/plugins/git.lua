dd0f8113a0d33e96a5307cc9a4c8b870dc278b49
Andrea Bergia <andreabergia@gmail.com>
2025-10-24 09:36:15 +0200

    Created 'ga' command for 'git add'

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,5 +12,23 @@ return {
     vim.keymap.set('n', '<leader>gg', function()
       neogit.open()
     end, { desc = 'Gui' })
+
+    vim.keymap.set('n', '<leader>ga', function()
+      local file = vim.api.nvim_buf_get_name(0)
+      if file == '' then
+        vim.notify('No file associated with this buffer', vim.log.levels.WARN)
+        return
+      end
+
+      local relpath = vim.fn.fnamemodify(file, ':.')
+      local result = vim.fn.system { 'git', 'add', relpath }
+
+      if vim.v.shell_error ~= 0 then
+        vim.notify('git add failed: ' .. vim.trim(result), vim.log.levels.ERROR)
+        return
+      end
+
+      vim.notify('git add ' .. relpath, vim.log.levels.INFO)
+    end, { desc = 'Add current file' })
   end,
 }

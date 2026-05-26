125efed8dca1a3f226265a03c2e18a67d364b10f
Andrea Bergia <andreabergia@gmail.com>
2026-05-26 16:22:36 +0200

    refactor: remove disabled copilot config

--- init.lua
+++ init.lua
@@ -51,7 +51,6 @@ require('lazy').setup {
   require 'plugins.flash',
   require 'plugins.todo-comment',
   require 'plugins.persistence',
-  -- require 'plugins.copilot',
   require 'plugins.neotest',
   require 'plugins.rustacean',
   require 'plugins.crates',
